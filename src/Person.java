a7bd0f91055746eb3d210d650374c230c50af556
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-17 14:40:58 +0200

    Ajout classe Person

---
+++ src/Person.java
@@ -0,0 +1,46 @@
+public class Person {
+    //attributs
+    public String lastName;
+    public String firstName;
+    public int age;
+    public String address;
+
+    public Person(String lastName, String firstName, int age, String address){
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+    //accesseurs
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress () {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
