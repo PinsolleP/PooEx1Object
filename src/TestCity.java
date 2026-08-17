bab19f25d94a141499c4f19475d47b0bb9914dfe
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-17 13:44:44 +0200

    Ajout pour test Rabbat

--- src/TestCity.java
+++ src/TestCity.java
@@ -3,13 +3,19 @@ public class TestCity {
        City toulouse = new City("Toulouse","France",450000);
        City bayonne = new City("Bayonne","France",150000);
        City madrid = new City("Madrid","Espagne",1100000);
+       City rabbat = new City("Rabbat", 577000);
 
        //System.out.println(toulouse.getName() + " " + toulouse.getCountry() + " " + toulouse.getPopulation());
        //System.out.println(bayonne.getName() + " " + bayonne.getCountry() + " " + bayonne.getPopulation());
        //System.out.println(madrid.getName() + " " + madrid.getCountry() + " " + madrid.getPopulation());
 
-        toulouse.setPopulation(-300);
-        System.out.println(toulouse);
+        //toulouse.setPopulation(370000);
+        //System.out.println(toulouse);
+
+        System.out.println(rabbat.getName() + " " + rabbat.getCountry() + " " + rabbat.getPopulation());
+        rabbat.setCountry("Maroc");
+        System.out.println(rabbat.getName() + " " + rabbat.getCountry() + " " + rabbat.getPopulation());
+
 
     }
 }
