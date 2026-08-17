87cf2609626d5b8f4675c88722cc843ce5b90d47
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-17 13:21:46 +0200

    Ajout Execption

--- src/TestCity.java
+++ src/TestCity.java
@@ -4,8 +4,12 @@ public class TestCity {
        City bayonne = new City("Bayonne","France",150000);
        City madrid = new City("Madrid","Espagne",1100000);
 
-       toulouse.display();
-       bayonne.display();
-       madrid.display();
+       //System.out.println(toulouse.getName() + " " + toulouse.getCountry() + " " + toulouse.getPopulation());
+       //System.out.println(bayonne.getName() + " " + bayonne.getCountry() + " " + bayonne.getPopulation());
+       //System.out.println(madrid.getName() + " " + madrid.getCountry() + " " + madrid.getPopulation());
+
+        toulouse.setPopulation(-300);
+        System.out.println(toulouse);
+
     }
 }
