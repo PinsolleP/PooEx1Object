1503c70c5964dc5240e16b047e3a9477a4d94fa3
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-17 11:18:34 +0200

    Attributs et constructeur City

--- src/City.java
+++ src/City.java
@@ -1,3 +1,12 @@
 public class City {
- 
+    //attributs
+    public String name;
+    public String country;
+    public int residents_number;
+
+    public City(String name, String country, int residents_number){
+        this.name = name;
+        this.country = country;
+        this.residents_number = residents_number;
+    }
 }
