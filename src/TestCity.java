2537923d41923c2468c297cde7318e7bfde896ab
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-17 14:27:22 +0200

    Ajout affichage avec tostring

--- src/TestCity.java
+++ src/TestCity.java
@@ -4,6 +4,8 @@ public class TestCity {
        City bayonne = new City("Bayonne","France",150000);
        City madrid = new City("Madrid","Espagne",1100000);
        City rabbat = new City("Rabbat", 577000);
+       City paris = new City("Paris","France",2000000);
+       City nice = new City("Nice", "France", 340000);
 
        //System.out.println(toulouse.getName() + " " + toulouse.getCountry() + " " + toulouse.getPopulation());
        //System.out.println(bayonne.getName() + " " + bayonne.getCountry() + " " + bayonne.getPopulation());
@@ -15,6 +17,9 @@ public class TestCity {
         System.out.println(rabbat.getName() + " " + rabbat.getCountry() + " " + rabbat.getPopulation());
         rabbat.setCountry("Maroc");
         System.out.println(rabbat.getName() + " " + rabbat.getCountry() + " " + rabbat.getPopulation());
+
+        toulouse.display();
+        System.out.println(toulouse);
 
 
     }
