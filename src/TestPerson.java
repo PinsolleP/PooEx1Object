b55256a678a7dbe8f48e871d2ab944cc17ac5a08
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-17 15:53:29 +0200

    Création des personnes

--- src/TestPerson.java
+++ src/TestPerson.java
@@ -1,11 +1,14 @@
 public class TestPerson {
     public static void main(String[] args){
         Person manu = new Person("Macron", "Emmanuel", 43, "Elysee à Paris", new City("Amiens","France", 0));
-        Person poutine = new Person("Poutine", "Vladimir", 68);
-        Person biden = new Person("Biden", "joe");
+        Person nico = new Person("Sarkozy", "Nicolas", 66, "habitant Paris", new City("Paris", "France", 2000000));
+        Person boris = new Person("Johnson", "Boris", 56, "habitant Downing street à London", new City("New York", "Etats_unis", 0));
+        Person gege = new Person("Depardieu", "Gerard", 72, "habitant Moscou", new City("Châteauroux", "France",0));
+        Person lenny = new Person("Kravitz", "lenny", 56, "habitant Hotel particulier à Paris", new City("New York", "Etats_unis", 0));
+        Person jennifer = new Person("Lawrence", "jennifer", 30, "habitant Louisville aux USA", new City("Indian Hills", "Etats_unis", 0));
 
-        System.out.println(manu);
-        System.out.println(poutine);
-        System.out.println(biden);
+        //System.out.println(manu);
+        //System.out.println(poutine);
+        //System.out.println(biden);
     }
 }
