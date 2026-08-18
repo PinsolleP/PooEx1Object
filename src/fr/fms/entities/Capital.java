0068414d09ae82e1fc7a74db582ec68b9a3f100b
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 15:25:05 +0200

    Ajout de la javadoc

--- src/fr/fms/entities/Capital.java
+++ src/fr/fms/entities/Capital.java
@@ -1,13 +1,30 @@
 package fr.fms.entities;
-
+/**
+ * Représente une capitale, qui est une ville possédant un monument.
+ *
+ * <p>Cette classe hérite de {@link City} et ajoute l'attribut
+ * correspondant au monument de la capitale.
+ *
+ */
 public class Capital extends City {
+    /** Monument associé à la capitale. */
     private String monument;
 
     public Capital(String monument, String name, String country, int population) {
+        /**
+         * Construit une capitale avec son monument, son nom, son pays
+         * et sa population.
+         *
+         * @param monument monument associé à la capitale
+         * @param name nom de la capitale
+         * @param country pays de la capitale
+         * @param population nombre d'habitants de la capitale
+         */
         super(name, country, population);
         this.monument = monument;
     }
 
+    // Accesseurs
     public String getMonument() { return monument ; }
 
     public void setMonument(String monument) {
@@ -15,6 +32,11 @@ public class Capital extends City {
     }
 
     public String toString() {
+        /**
+         * Retourne une représentation textuelle de la capitale.
+         *
+         * @return le nom, le pays et le monument de la capitale
+         */
         return  getName() + " , " +
                 getCountry() + " , " +
                 getMonument();
