b7e5c5ee4027fda222b5f58b1c2a0eff3b6028e4
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 11:33:40 +0200

    Ajout de la méthode tostring

--- src/fr/fms/entities/Employee.java
+++ src/fr/fms/entities/Employee.java
@@ -2,9 +2,9 @@ package fr.fms.entities;
 
 public class Employee extends Person {
     private String company;
-    private int salary;
+    private double salary;
 
-    public Employee (String company, int salary, String lastName, String firstName, int age, String address, City bornCity ){
+    public Employee (String company, double salary, String lastName, String firstName, int age, String address, City bornCity ){
         super(lastName, firstName, age, address, bornCity);
         this.company = company;
         this.salary = salary;
@@ -16,9 +16,15 @@ public class Employee extends Person {
         this.company = company;
     }
 
-    public int getSalary() { return salary ; }
+    public double getSalary() { return salary ; }
 
-    public void setSalary(int salary) {
+    public void setSalary(double salary) {
         this.salary = salary;
+    }
+
+    public String toString() {
+        return  super.toString() + " , Entreprise : "
+                + getCompany() + " , salaire : "
+                + getSalary();
     }
 }
