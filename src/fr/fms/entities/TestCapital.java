9ae98ad7f43c864723f0f4913568730f894982ad
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 15:27:54 +0200

    Ajout de la javadoc

--- src/fr/fms/entities/TestCapital.java
+++ src/fr/fms/entities/TestCapital.java
@@ -1,12 +1,23 @@
 package fr.fms.entities;
-
+/**
+ * Classe de test de la classe {@link Capital}.
+ *
+ * <p>Permet de tester la création et l'affichage de plusieurs capitales.</p>
+ *
+ */
 public class TestCapital {
-
+    /**
+     * Point d'entrée du programme.
+     *
+     * @param args arguments passés au programme (non utilisé ici)
+     */
     public static void main(String[] args){
 
+        // Création de plusieurs capitales
         Capital paris = new Capital("Tour Eiffel", "Paris", "France", 2000000);
         Capital londres = new Capital("Bigben", "Londres", "GB", 9400000);
 
+        // Affichage des capitales
         System.out.println(paris);
         System.out.println(londres);
     }
