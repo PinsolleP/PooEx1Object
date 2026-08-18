80cca006a68da2f776d6feef6cc3cc08b1163618
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 15:35:27 +0200

    Ajout de la javadoc

--- src/fr/fms/entities/TestCommercial.java
+++ src/fr/fms/entities/TestCommercial.java
@@ -1,13 +1,27 @@
 package fr.fms.entities;
-
+/**
+ * Classe de test de la classe {@link Commercial}.
+ *
+ * <p>Permet de tester la création et l'affichage d'un commercial
+ * ainsi que l'utilisation de sa ville de naissance.</p>
+ *
+ */
 public class TestCommercial {
+    /**
+     * Point d'entrée du programme.
+     *
+     * @param args arguments passés au programme (non utilisé ici)
+     */
     public static void main(String[] args) {
 
+        // Création de la ville de naissance
         City limoges = new City("Limoges", "France", 133000);
 
+        // Création du commercial
         Commercial robert = new Commercial("brico", 5.0, "Dupont",
                 "robert", 50, "habitant rue des rosiers à Toulouse", limoges);
 
+        // Affichage du commercial
         System.out.println(robert);
     }
 }
