54d6229ab87aca6ea2457e87dacdeb0e8f5a4158
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 09:50:59 +0200

    Test affichage Capitale

--- src/fr/fms/entities/TestCapital.java
+++ src/fr/fms/entities/TestCapital.java
@@ -1,4 +1,13 @@
 package fr.fms.entities;
 
 public class TestCapital {
+
+    public static void main(String[] args){
+
+        Capital paris = new Capital("Tour Eiffel", "Paris", "France", 2000000);
+        Capital londres = new Capital("Bigben", "Londres", "GB", 9400000);
+
+        System.out.println(paris);
+        System.out.println(londres);
+    }
 }
