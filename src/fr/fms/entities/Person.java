7d5b16067ca6c6bab3ef5e873bb6fce166d95fd4
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 15:19:27 +0200

    Ajout de la javadoc

--- src/fr/fms/entities/Person.java
+++ src/fr/fms/entities/Person.java
@@ -1,5 +1,12 @@
 package fr.fms.entities;
-
+/**
+ * Représente une personne avec son identité, son âge, son adresse
+ * et sa ville de naissance.
+ *
+ * Cette classe est abstraite et sert de classe mère à des classes
+ * spécialisées telles que Employee et Commercial.
+ *
+ */
 public abstract class Person {
     //attributs
     public String lastName;
@@ -9,6 +16,15 @@ public abstract class Person {
     public City bornCity;
 
     public Person(String lastName, String firstName, int age, String address, City bornCity) {
+        /**
+         * Construit une personne avec l'ensemble de ses informations.
+         *
+         * @param lastName nom de famille
+         * @param firstName prénom
+         * @param age âge
+         * @param address adresse
+         * @param bornCity ville de naissance
+         */
         this.age = age;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -16,12 +32,27 @@ public abstract class Person {
         this.bornCity = bornCity;
     }
     public Person(String lastName, String firstName, int age ) {
+        /**
+         * Construit une personne avec son nom, son prénom et son âge.
+         * L'adresse est initialisée à "unknown".
+         *
+         * @param lastName nom de famille
+         * @param firstName prénom
+         * @param age âge
+         */
         this.age = age;
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = "unknown";
     }
     public Person(String lastName, String firstName) {
+        /**
+         * Construit une personne avec son nom et son prénom.
+         * L'âge est initialisé à 0 et l'adresse à "unknown".
+         *
+         * @param lastName nom de famille
+         * @param firstName prénom
+         */
         this.age = 0;
         this.firstName = firstName;
         this.lastName = lastName;
@@ -70,12 +101,25 @@ public abstract class Person {
     }
 
     public String toString() {
+        /**
+         * Retourne une représentation textuelle de la personne.
+         *
+         * @return les informations principales de la personne
+         */
         return  getLastName() + " , " +
                 getFirstName() + " , " +
                 getAge() + " , " +
                 getAddress() + " , " +
                 getBornCity();
         }
+    /**
+     * Calcule la rémunération de la personne.
+     *
+     * Cette méthode doit être implémentée par les classes filles,
+     * car le calcul dépend du type de personne.
+     *
+     * @return la rémunération calculée
+     */
     public abstract double remuneration();
 
     }
