55c3edf9cce6c96b4eda00ed313f876f1d1cb79c
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 15:14:45 +0200

    Ajout de la javadoc

--- src/TestCity.java
+++ src/TestCity.java
@@ -1,6 +1,16 @@
 import fr.fms.entities.City;
-
+/**
+ * Classe de test de la classe {@link City}.
+ * Permet de tester la création, la modification et l'affichage
+ * des objets City.
+ *
+ */
 public class TestCity {
+    /**
+     * Point d'entrée du programme.
+     *
+     * @param args arguments passés au programme
+     */
     public static void main(String[] args){
        City toulouse = new City("Toulouse","France",450000);
        City bayonne = new City("Bayonne","France",150000);
@@ -9,21 +19,26 @@ public class TestCity {
        City paris = new City("Paris","France",2000000);
        City nice = new City("Nice", "France", 340000);
 
+       //Test des accesseurs
        //System.out.println(toulouse.getName() + " " + toulouse.getCountry() + " " + toulouse.getPopulation());
        //System.out.println(bayonne.getName() + " " + bayonne.getCountry() + " " + bayonne.getPopulation());
        //System.out.println(madrid.getName() + " " + madrid.getCountry() + " " + madrid.getPopulation());
 
+        //Test de modification de la population
         //toulouse.setPopulation(370000);
         //System.out.println(toulouse);
 
+        //Test de modification du pays
         System.out.println(rabbat.getName() + " " + rabbat.getCountry() + " " + rabbat.getPopulation());
         rabbat.setCountry("Maroc");
         System.out.println(rabbat.getName() + " " + rabbat.getCountry() + " " + rabbat.getPopulation());
 
+        //Test de la méthode display()
         toulouse.display();
+        //Test de la méthode tostring
         System.out.println(toulouse);
+        //Affichage du nombre d'instances de City
         System.out.println(City.compteur);
-
 
     }
 }
