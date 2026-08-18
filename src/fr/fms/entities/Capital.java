37e22167571546a5196170976f5679e1465cbc00
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 09:51:58 +0200

    Ajout getter/setter et toString

--- src/fr/fms/entities/Capital.java
+++ src/fr/fms/entities/Capital.java
@@ -1,10 +1,25 @@
 package fr.fms.entities;
 
-public class Capital extends City{
+public class Capital extends City {
     private String monument;
 
-    public Capital(String monument, String name, String country, int population){
-        super(name, country,population);
+    public Capital(String monument, String name, String country, int population) {
+        super(name, country, population);
         this.monument = monument;
     }
+
+    public String getMonument() { return monument ; }
+
+    public void setMonument(String monument) {
+        this.monument = monument;
+    }
+
+    public String toString() {
+        return  getName() + " , " +
+                getCountry() + " , " +
+                getPopulation() + " d'habitants monument : " +
+                getMonument();
+    }
 }
+
+
