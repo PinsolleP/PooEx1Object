4955a11c6949aa70e3efdabd211c2802f6675a22
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-18 15:04:34 +0200

    eclaircir les erreurs

--- src/TestPerson.java
+++ src/TestPerson.java
@@ -2,7 +2,6 @@ import fr.fms.entities.City;
 import fr.fms.entities.Person;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class TestPerson {
     public static void main(String[] args) {
